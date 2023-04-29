78846ae71b6da5103066ec02190ddc50513889f0
homayk228 <43516026+homayk228@users.noreply.github.com>
2023-04-29 21:41:33 +0700

    V1 Release!
    Added converter and example files.

---
+++ conv.py
@@ -0,0 +1,46 @@
+notes=[]
+duration=[]
+delaynotes=[]
+def parsenotes():
+        inputfile="./"+input("Вход:")
+        f = open(inputfile)
+        for line in f:
+                parslin=line[4:9]
+                #print(parslin)
+                if parslin=="tone(":
+                        #print("note!",line[18:21])
+                        notes.append(line[18:21])
+                        duration.append(str(line[23:29]).replace(');','',1).replace(')','',1))
+                elif parslin=="delay":
+                        #print("duration",line[10:22])
+                        delaynotes.append(str(line[10:22]).replace(');\n','',1))
+        f.close()
+        print("Готово!")
+        return
+
+def output():
+        outputfile="./"+input("Выход:")
+        f=open(outputfile,"w")
+        f.write(str(notes)+'\n')
+        f.write(str(duration)+'\n')
+        f.write(str(delaynotes)+'\n')
+        print("Готово!")
+        return
+
+print("Конвертер tone-duration-delay спагетити в отдельные массивы")
+while(1):
+	print("Выберите режим:")
+	print("1:Ввод txt с tone-delay конструкциями")
+	print("2:Вывод массивов в файл")
+	print("3:Выход")
+	choice=int(input(":"))
+	if choice==1:
+		parsenotes()
+	elif choice==2:
+		output()
+	elif choice==3:
+		break
+	else:
+		print("Фигня какая-то, вводи по новой")
+print("Давай!")
+
