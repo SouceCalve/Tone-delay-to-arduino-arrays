a19db7a25a50b5cdbd1b06240c5c4511fb98e209
homayk228 <43516026+homayk228@users.noreply.github.com>
2023-05-24 17:17:12 +0700

    Add a status and some more info
    Added:
    1)Program status
    2) The size of the array when compiling
    3) Sample playback time

--- conv.py
+++ conv.py
@@ -2,7 +2,17 @@ notes=[]
 duration=[]
 delaynotes=[]
 delayprev=False
+tonecounter=0
+delaycounter=0
+def sumforcheck(name):
+	sum=0
+	for i in name:
+		sum+=float(i)
+	return(sum/1000)
+
 def parsenotes():
+	global delaycounter
+	global tonecounter
 	inputfile="./"+input("Вход:")
 	f = open(inputfile)
 	for line in f:
@@ -13,14 +23,16 @@ def parsenotes():
 			notes.append(line[18:21])
 			duration.append(str(line[23:29]).replace(');','',1).replace(')','',1))
 			delayprev=False
+			tonecounter+=1
 		elif parslin=="delay":
 			if delayprev==True:
 				#print(line[10:24])
 				delaynotes[-1]=str(float(delaynotes[-1])+float(str(line[10:24]).replace(');','',1).replace(')','',1).replace('\n','')))
 			if delayprev==False:
 				#print("duration",line[10:22])
-				delaynotes.append(str(line[10:24]).replace(');','',1).replace(')','',1).replace('\n','')) #добавить проверку множественых delay'в
+				delaynotes.append(str(line[10:24]).replace(');','',1).replace(')','',1).replace('\n',''))
 				delayprev=True
+			delaycounter+=1
 	f.close()
 	print("Готово!")
 	return
@@ -34,8 +46,25 @@ def output():
         print("Готово!")
         return
 
-print("Конвертер tone-duration-delay спагетити в отдельные массивы")
+print("Конвертер tone-duration-delay спагетти в отдельные массивы")
 while(1):
+	print("")
+	if (tonecounter>0 and delaycounter>0):
+		print("Статус: Массив загружен, ожидание вывода...")
+		print("Размер массивов в памяти: "+str(2*tonecounter+4*tonecounter+4*delaycounter)+"KB")
+		if(sumforcheck(duration)>sumforcheck(delaynotes)):
+			if round(sumforcheck(duration))>60:
+				print("Длительность мелодии: "+str(round(sumforcheck(duration))/60)+" минут "+str(round(sumforcheck(duration))%60)+" секунд")
+			else:
+				print("Длительность мелодии: "+str(round(sumforcheck(duration)))+" секунд")
+		else:
+			if round(sumforcheck(delaynotes))>60:
+				print("Длительность мелодии: "+str(round(sumforcheck(delaynotes))/60)+" минут "+str(round(sumforcheck(delaynotes))%60)+" секунд")
+			else:
+				print("Длительность мелодии: "+str(round(sumforcheck(delaynotes)))+" секунд") 
+	else:
+		print("Статус: Массив ещё не загружен. Выполните ввод файла.")
+	print("")
 	print("Выберите режим:")
 	print("1:Ввод txt с tone-delay конструкциями")
 	print("2:Вывод массивов в файл")
